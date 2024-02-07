ce8fa687be08058b0cdfeef2e4f2c9f4291a86ed
Gerson Daniel Guerrero Castillo <USSS037423@ugb.edu.sv>
2024-02-07 08:18:41 -0600

    Conversores App

--- Calculadora/app/src/main/java/com/ugb/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/ugb/calculadora/MainActivity.java
@@ -1,36 +1,61 @@
 package com.ugb.calculadora;
 
-import static com.ugb.calculadora.R.*;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-TabHost tbh;
 
-    TextView txt;
+    TabHost tbh;
+    TextView tempVal;
     Button btn;
-
     Spinner spn;
-
+    conversores objConversor = new conversores();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-     /*a
-        tbh=findViewById(R.id.tbhconversor);
+        tbh = findViewById(R.id.tbhConversores);
         tbh.setup();
-        tbh.addTab(tbh.newTabSpec("LONGITUS").setContent(id.tabLongitud).setIndicator("LONGITUD",null));
-        tbh.addTab(tbh.newTabSpec("ALMACENAMIENTO").setContent(id.tabAlmacenemiento).setIndicator("ALMACENAMIENTO",null));
-        tbh.addTab(tbh.newTabSpec("MONEDAS").setContent(id.tabMonedas).setIndicator("MONEDAS",null));
-*/
 
+        tbh.addTab(tbh.newTabSpec("LONGITUD").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("ALMACENAMIENTO").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+        tbh.addTab(tbh.newTabSpec("MONEDAS").setContent(R.id.tabMonedas).setIndicator("MONEDAS",null));
+   btn=findViewById(R.id.btnLongitudConvertir);
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            spn=findViewById(R.id.spnLongitudDe);
+            int de =spn.getSelectedItemPosition();
+
+            spn=findViewById(R.id.spnLongitudA);
+            int a =spn.getSelectedItemPosition();
+
+            tempVal=findViewById(R.id.txtLongitudCantidad);
+            double cantidad= Double.parseDouble(tempVal.getText().toString());
+
+            double resp = objConversor.convertir(0,de,a,cantidad);
+            Toast.makeText(getApplicationContext(),"Respuesta: "+resp,Toast.LENGTH_LONG).show();
+        }
+    });
+    }
+}
+class conversores{
+    double [][] valores = {
+            {1,100,39.3701,3.2808399,1.193,1.0936133,0.001,0.00062137}
+    };
+
+    public  double convertir(int opcion, int de, int a, double cantidad){
+
+        return valores [opcion][a]/valores[opcion][de]*cantidad;
     }
 }
